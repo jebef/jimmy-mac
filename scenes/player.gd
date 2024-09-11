159517c8cf207bc13d3e64102eb7e0fdaae28d2d
Wyatt Jebef <wjebef@berkeley.edu>
2024-09-11 10:44:01 -0700

    Incorporated jump animation, small bug with timing

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,31 +4,56 @@ extends CharacterBody2D
 
 # Global vars 
 var direction : float = 0.0
-const SPEED = 75.0
-const JUMP_VELOCITY = -250.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -200.0
 
 func _ready():
 	animation_tree.active = true
 	
 # A function to update advanced conditions for our animation tree 
 func update_animation_parameters():
-	# Check if the player is moving and update params accordingly 
-	if velocity == Vector2.ZERO:
+	# IDLE
+	if is_on_floor() and velocity == Vector2.ZERO:
 		animation_tree["parameters/conditions/is_idle"] = true
-		animation_tree["parameters/conditions/is_moving"] = false
+		animation_tree["parameters/conditions/is_running"] = false
+	# RUN 
 	else:
 		animation_tree["parameters/conditions/is_idle"] = false
-		animation_tree["parameters/conditions/is_moving"] = true
+		animation_tree["parameters/conditions/is_running"] = true
 		
-	animation_tree["parameters/conditions/is_attacking"] = false
-	if Input.is_action_just_pressed("attack"):
-		animation_tree["parameters/conditions/is_attacking"] = true
+	# JUMP
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animation_tree["parameters/conditions/is_jumping"] = true
+	else:
+		animation_tree["parameters/conditions/is_jumping"] = false
+	# LAND
+	if animation_tree["parameters/conditions/is_falling"] and is_on_floor():
+		animation_tree["parameters/conditions/is_landing"] = true 
+	else:
+		animation_tree["parameters/conditions/is_landing"] = false
+	# FALL
+	if not is_on_floor() and velocity.y >= 0.0:
+		animation_tree["parameters/conditions/is_falling"] = true 
+	else:
+		animation_tree["parameters/conditions/is_falling"] = false
+ 
+	
+	
+		
+		
+	
 	
 	# Update blend params if necessary 
 	if direction:
 		animation_tree["parameters/Idle/blend_position"] = direction
 		animation_tree["parameters/Run/blend_position"] = direction
-		animation_tree["parameters/Attack/blend_position"] = direction
+		#animation_tree["parameters/Attack/blend_position"] = direction
+		animation_tree["parameters/Jump/blend_position"] = direction
+		animation_tree["parameters/JumpLoop/blend_position"] = direction
+		animation_tree["parameters/Fall/blend_position"] = direction
+		animation_tree["parameters/FallLoop/blend_position"] = direction
+		animation_tree["parameters/Land/blend_position"] = direction
+
 		
 	
 	
@@ -36,7 +61,7 @@ func update_animation_parameters():
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 0.25
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
