0d351e5c5c435d4962821758dfd49d7571a3c4db
Wyatt Jebef <wjebef@berkeley.edu>
2024-09-14 15:54:24 -0700

    Added first version of melee animation, started melee script

--- scripts/GroundState.gd
+++ scripts/GroundState.gd
@@ -4,11 +4,16 @@ class_name GroundState
 
 const JUMP_VELOCITY : float = -100.0
 @export var air_state : State 
+@export var attack_state : State
 @export var jump_animation_name : String = ""
+@export var melee_animation_name : String = ""
 
 func state_input(event : InputEvent):
 	if event.is_action_pressed("jump"):
 		jump()
+	
+	if event.is_action_pressed("attack"):
+		melee()
 		
 func state_process(delta : float) -> void:
 	if not character.is_on_floor():
@@ -18,4 +23,9 @@ func jump():
 	character.velocity.y = JUMP_VELOCITY
 	next_state = air_state
 	playback.travel(jump_animation_name)
+	
+func melee():
+	next_state = attack_state
+	playback.travel(melee_animation_name)
+	
 	
